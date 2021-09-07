d3d46b1af48077dbdfd9e3bec96b1a0db48a4bba
coding-cuber <73984595+coding-cuber@users.noreply.github.com>
2021-09-07 18:14:00 +1000

    began game code

---
+++ games/2048/objects.py
@@ -0,0 +1,27 @@
+import discord
+
+
+class Grid:
+    def __init__(self, size: int, default_char: str):
+        self.size = size
+        self.default_char = default_char
+
+        self.rows = self.new_grid()
+
+    def new_grid(self):
+        rows = []
+
+        count = 0
+        for x in range(self.size):
+            rows.append([])
+            for y in range(self.size):
+                rows[count].append(self.default_char + " ")
+
+            count += 1
+
+        return rows
+
+    def send_grid(self, ctx):
+        for x in self.rows:
+            for y in x:
+                await ctx.channel.send(y)
